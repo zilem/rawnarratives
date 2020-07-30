69f1a3a4c82174ea2e03b416f8f08b0dca18669f
Zi Lem <story@zilem.studio>
2020-07-30 09:02:05 +0800

    07/30 9:02

--- js/share.js
+++ js/share.js
@@ -13,7 +13,7 @@ function tHShare(post_title) {
   window.open ('//twitter.com/intent/tweet?text=' + post_title + '%0A' + location.href.replace(location.hash,""));
 }
 function teHShare(post_title) {
-  window.open ('//t.me/share/url?url='+ location.href.replace(location.hash,"") + '%0A&text=' + post_title + '%0A');
+  window.location = '//t.me/share/url?url='+ location.href.replace(location.hash,"") + '&text=' + post_title + '%0A';
 }
 function fHShare(post_title) {
   window.open ('//facebook.com/sharer/sharer.php?quote=' + post_title + '&u=' + location.href.replace(location.hash,""));
