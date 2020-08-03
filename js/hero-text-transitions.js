1505cf934324a557561b6593437a058b4936e507
Zi Lem <story@zilem.studio>
2020-08-03 09:39:52 +0800

    08/03 9:40

--- js/hero-text-transitions.js
+++ js/hero-text-transitions.js
@@ -1,9 +1,28 @@
-$('.hero-slider').slick({
-    slidesToShow: 1,
-    prevArrow: $(".themes-carousel-controls .prev"),
-    nextArrow: $(".themes-carousel-controls .next"),
-    autoplay: true,
-    dots: false,
-    autoplaySpeed: 7000,
-    fade: true
-  });
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
